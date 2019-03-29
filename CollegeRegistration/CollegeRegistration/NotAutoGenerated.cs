cc6b34763573b5b24acebbadbd404182c9e6dc3a
Noel <noeljensen10@gmail.com>
2019-03-28 22:52:16 -0400

    fixed it - we all have to go through the same steps

---
+++ CollegeRegistration/CollegeRegistration/NotAutoGenerated.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeRegistration
+{
+    public partial class Student
+    {
+        public string BetterDisplay => $"{Name} {Major.Name} {Major.College}";
+    }
+
+    public partial class Major
+    {
+        public string NameAndCollege => $"{Name} {College}";
+    }
+}
---
+++ RegistrationDatabase/RegistrationDatabase/Database1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RegistrationDatabase
+{
+     public partial class Database1 : Form
+     {
+          public Database1()
+          {
+               InitializeComponent();
+          }
+     }
+}
